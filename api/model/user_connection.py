c085ca304922de6dd2809e45aa7846e9cced9cb9
Miguel Angel López <admon@tecnott.com.mx>
2023-08-23 17:10:30 -0600

    Corrections of the files

--- api/model/user_connection.py
+++ api/model/user_connection.py
@@ -17,7 +17,7 @@ class UserConnection():
     def __init__(self):
         try:
             self.conn = psycopg.connect(
-                dbname=DB_HOST,
+                dbname=DB_NAME,
                 user=DB_USER,
                 password = DB_PASSWORD,
                 host=DB_HOST,
@@ -29,29 +29,7 @@ class UserConnection():
 
     #mostrar publicaciones segun pais de usuario
     def read_publications_by_country(self, country):
-        with self.conn.cursos() as cur:
-            data = cur.execute("""
-                SELECT * FROM publications INNER JOIN users ON publications.id = users.id WHERE country = %s
-            """, (country,))
-            return data.fetchall()
-
-    #destructor class
-    def __def__(self):
-        self.conn.close()
-    
-    def write(self, data):
         with self.conn.cursor() as cur:
-            cur.execute("""
-                            INSERT INTO "users"(email, pass_user, country) VALUES(%(email)s, %(password)s, %(country)s)
-                        """, data)
-        self.conn.commit()
-    #destructor class
-    def __def__(self):
-        self.conn.close()
-
-    #mostrar publicaciones segun pais de usuario
-    def read_publications_by_country(self, country):
-        with self.conn.cursos() as cur:
             data = cur.execute("""
                 SELECT * FROM publications INNER JOIN users ON publications.id = users.id WHERE country = %s
             """, (country,))
