9225d1152658942c6eb64a96015d95e0a46bca04
Miguel Angel López <admon@tecnott.com.mx>
2023-08-23 15:43:23 -0600

    Corrections of the files

--- api/schema/user_schema.py
+++ api/schema/user_schema.py
@@ -1,9 +1,8 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from typing import Optional
 
 class UserSchema(BaseModel):
     user_id: Optional[int]
-    email: str
+    email: EmailStr
     pass_user: str
     country: str
-
