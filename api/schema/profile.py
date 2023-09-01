7e203a41f22834f755f14d45904207ad747ba40a
rodrjm <rodrjm94@gmail.com>
2023-09-01 09:34:28 -0300

    API profile
    Creación de APIs para el perfil

--- api/schema/profile.py
+++ api/schema/profile.py
@@ -3,21 +3,22 @@ from schema.user import User
 
 
 class ProfileBase(BaseModel):
+    id: int
     name: str
     phone: str
     state: str
     province: str
     postal_code: str
+    user_id: User
 
     class Config:
         from_attributes = True
         from_orm = True
+
 
 class ProfileCreate(ProfileBase):
     pass
 
 
 class Profile(ProfileBase):
-    profile_id: int
-    user_id: int
-    users: list[User]
+    pass
